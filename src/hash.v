9b13423558949e92ea47811b8c680e4f192b25e1
sora <sora@wide.ad.jp>
2012-03-30 18:52:25 +0900

    refactoring

--- src/hash.v
+++ src/hash.v
@@ -8,17 +8,18 @@
 
 module hash_r1 #(
   parameter maxwords = 250                  // MAX number of key length.
-, parameter nloop    = div12p1(maxwords)
+, parameter nloop    = 21
+//, parameter nloop    = div12p1(maxwords)
 )(
   input CLK
 , input RST
 , input [31:0] ia
 , input [31:0] ib
 , input [31:0] ic
+, input [7:0]  iw
 , input [31:0] k0
 , input [31:0] k1
 , input [31:0] k2
-, input [7:0]  iw
 , output reg[31:0] oa
 , output reg[31:0] ob
 , output reg[31:0] oc
@@ -33,24 +34,22 @@ begin
 end
 endfunction
 
-reg[31:0] a0, b0, c0;
-reg[31:0] a1, b1, c1;
-reg[31:0] a2, b2, c2;
-reg[31:0] a3, b3, c3;
-reg[7:0]  w0, w1, w2, w3;
-
-wire[7:0]  next_w0 = iw - 8'd12;
-wire[31:0] next_a0 = ia + k0;
-wire[31:0] next_b0 = ib + k1;
-wire[31:0] next_c0 = ic + k2;
-wire[31:0] next_a1 = (a0 - c0) ^ {c0[27:0], c0[31:28]};
-wire[31:0] next_c1 = c0 + b0;
-wire[31:0] next_a2 = a1 + c1;
-wire[31:0] next_c2 = (c1 - b1) ^ {b1[23:0], b1[31:24]};
-wire[31:0] next_a3 = (a2 - c2) ^ {c2[15:0], c2[31:16]};
-wire[31:0] next_c3 = c2 + b2;
-wire[31:0] next_a4 = a3 + c3;
-wire[31:0] next_c4 = (c3 - b3) ^ {b3[27:0], b3[31:28]};
+wire[7:0]  w0 = iw - 8'd12;
+wire[31:0] a0 = ia + k0;
+wire[31:0] b0 = ib + k1;
+wire[31:0] c0 = ic + k2;
+wire[31:0] a1 = (a0 - c0) ^ {c0[27:0], c0[31:28]};
+wire[31:0] c1 = c0 + b0;
+wire[31:0] b1 = (b0 - a1) ^ {a1[25:0], a1[31:26]};
+wire[31:0] a2 = a1 + c1;
+wire[31:0] c2 = (c1 - b1) ^ {b1[23:0], b1[31:24]};
+wire[31:0] b2 = b1 + a2;
+wire[31:0] a3 = (a2 - c2) ^ {c2[15:0], c2[31:16]};
+wire[31:0] c3 = c2 + b2;
+wire[31:0] b3 = (b2 - a3) ^ {a3[12:0], a3[31:13]};
+wire[31:0] a4 = a3 + c3;
+wire[31:0] c4 = (c3 - b3) ^ {b3[27:0], b3[31:28]};
+wire[31:0] b4 = b3 + a4;
 
 always @(posedge CLK) begin
   if (RST) begin
@@ -60,47 +59,15 @@ always @(posedge CLK) begin
     ow <= 8'b0;
   end else begin
     if (iw > 8'd12) begin
-      a0 <= next_a0;
-      c0 <= next_c0;
-      b0 <= next_b0;
-      w0 <= next_w0;
-      a1 <= next_a0;
-      c1 <= next_c1;
-      b1 <= (b0 - next_a1) ^ {next_a1[25:0], next_a1[31:26]};
-      w1 <= w0;
-      a2 <= next_a2;
-      c2 <= next_c2;
-      b2 <= b1 + next_a2;
-      w2 <= w1;
-      a3 <= next_a3;
-      c3 <= next_c3;
-      b3 <= (b2 - next_a3) ^ {next_a3[12:0], next_a3[31:13]};
-      w3 <= w2;
-      oa <= next_a4;
-      ob <= next_c4;
-      oc <= b3 + next_a4;
-      ow <= w3;
+      oa <= a4;
+      oc <= c4;
+      ob <= b4;
+      ow <= w0;
     end else begin
-      a0 <= ia;
-      c0 <= ic;
-      b0 <= ib;
-      w0 <= iw;
-      a1 <= a0;
-      c1 <= c0;
-      b1 <= b0;
-      w1 <= w0;
-      a2 <= a1;
-      c2 <= c1;
-      b2 <= b1;
-      w2 <= w1;
-      a3 <= a2;
-      c3 <= c2;
-      b3 <= b2;
-      w3 <= w2;
-      oa <= a3;
-      ob <= b3;
-      oc <= c3;
-      ow <= w3;
+      oa <= ia;
+      ob <= ib;
+      oc <= ic;
+      ow <= iw;
     end
   end
 end
@@ -112,49 +79,31 @@ module hash_r2 (
 , input [31:0] ia
 , input [31:0] ib
 , input [31:0] ic
+, input [7:0]  iw
 , input [31:0] k0
 , input [31:0] k1
 , input [31:0] k2
-, input [7:0]  iw
 , output reg[31:0] o
 );
 
 reg[31:0] a0, b0, c0;
-reg[31:0] a1, b1, c1;
-reg[31:0] a2, b2, c2;
-reg[31:0] next_a0, next_b0, next_c0;
 
-wire[31:0] next_c1 = (c0 ^ b0) - {b0[17:0], b0[31:18]};
-wire[31:0] next_a1 = (a0 ^ next_c1) - {next_c1[20:0], next_c1[31:21]};
-wire[31:0] next_c2 = (c1 ^ b1) - {b1[15:0], b1[31:16]};
-wire[31:0] next_a2 = (a1 ^ next_c2) - {next_c2[27:0], next_c2[31:28]};
+wire[31:0] c1 = (c0 ^ b0) - {b0[17:0], b0[31:18]};
+wire[31:0] a1 = (a0 ^ c1) - {c1[20:0], c1[31:21]};
+wire[31:0] b1 = (b0 ^ a1) - {a1[6:0], a1[31:7]};
+wire[31:0] c2 = (c1 ^ b1) - {b1[15:0], b1[31:16]};
+wire[31:0] a2 = (a1 ^ c2) - {c2[27:0], c2[31:28]};
+wire[31:0] b2 = (b1 ^ a2) - {a2[17:0], a2[31:18]};
+wire[31:0] c3 = (c2 ^ b2) - {b2[7:0], b2[31:8]};
 
 always @(posedge CLK) begin
   if (RST)
-    o <= 1'b0;
+    o <= 32'b0;
   else begin
     if (iw) begin
-      c0 <= next_c0;
-      a0 <= next_a0;
-      b0 <= next_b0;
-      c1 <= next_c1;
-      a1 <= next_a1;
-      b1 <= (b0 ^ next_a1) - {next_a1[6:0], next_a1[31:7]};
-      c2 <= next_c2;
-      a2 <= next_a2;
-      b2 <= (b1 ^ next_a2) - {next_a2[17:0], next_a2[31:18]};
-      o  <= (c2 ^ b2) - {b2[7:0], b2[31:8]};
+      o <= c3;
     end else begin
-      c0 <= ic;
-      a0 <= ia;
-      b0 <= ib;
-      c1 <= c0;
-      a1 <= a0;
-      b1 <= b0;
-      c2 <= c1;
-      a2 <= a1;
-      b2 <= b1;
-      o  <= c2;
+      o <= ic;
     end
   end
 end
@@ -162,69 +111,64 @@ end
 always @* begin
   case (iw)
     8'd12: begin
-      next_c0 <= ic + k2;
-      next_b0 <= ib + k1;
-      next_a0 <= ia + k0;
+      c0 <= ic + k2;
+      b0 <= ib + k1;
+      a0 <= ia + k0;
     end
     8'd11: begin
-      next_c0 <= ic + k2 & 32'h00FFFFFF;
-      next_b0 <= ib + k1;
-      next_a0 <= ia + k0;
+      c0 <= ic + k2 & 32'h00FFFFFF;
+      b0 <= ib + k1;
+      a0 <= ia + k0;
     end
     8'd10: begin
-      next_c0 <= ic + k2 & 32'h0000FFFF;
-      next_b0 <= ib + k1;
-      next_a0 <= ia + k0;
+      c0 <= ic + k2 & 32'h0000FFFF;
+      b0 <= ib + k1;
+      a0 <= ia + k0;
     end
     8'd9: begin
-      next_c0 <= ic + k2 & 32'h000000FF;
-      next_b0 <= ib + k1;
-      next_a0 <= ia + k0;
+      c0 <= ic + k2 & 32'h000000FF;
+      b0 <= ib + k1;
+      a0 <= ia + k0;
     end
     8'd8: begin
-      next_c0 <= ic;
-      next_b0 <= ib + k1;
-      next_a0 <= ia + k0;
+      c0 <= ic;
+      b0 <= ib + k1;
+      a0 <= ia + k0;
     end
     8'd7: begin
-      next_c0 <= ic;
-      next_b0 <= ib + k1 & 32'h00FFFFFF;
-      next_a0 <= ia + k0;
+      c0 <= ic;
+      b0 <= ib + k1 & 32'h00FFFFFF;
+      a0 <= ia + k0;
     end
     8'd6: begin
-      next_c0 <= ic;
-      next_b0 <= ib + k1 & 32'h0000FFFF;
-      next_a0 <= ia + k0;
+      c0 <= ic;
+      b0 <= ib + k1 & 32'h0000FFFF;
+      a0 <= ia + k0;
     end
     8'd5: begin
-      next_c0 <= ic;
-      next_b0 <= ib + k1 & 32'h000000FF;
-      next_a0 <= ia + k0;
+      c0 <= ic;
+      b0 <= ib + k1 & 32'h000000FF;
+      a0 <= ia + k0;
     end
     8'd4: begin
-      next_c0 <= ic;
-      next_b0 <= ib;
-      next_a0 <= ia + k0;
+      c0 <= ic;
+      b0 <= ib;
+      a0 <= ia + k0;
     end
     8'd3: begin
-      next_c0 <= ic;
-      next_b0 <= ib;
-      next_a0 <= ia + k0 & 32'h00FFFFFF;
+      c0 <= ic;
+      b0 <= ib;
+      a0 <= ia + k0 & 32'h00FFFFFF;
     end
     8'd2: begin
-      next_c0 <= ic;
-      next_b0 <= ib;
-      next_a0 <= ia + k0 & 32'h0000FFFF;
+      c0 <= ic;
+      b0 <= ib;
+      a0 <= ia + k0 & 32'h0000FFFF;
     end
     8'd1: begin
-      next_c0 <= ic;
-      next_b0 <= ib;
-      next_a0 <= ia + k0 & 32'h000000FF;
-    end
-    default: begin
-      next_c0 <= ic;
-      next_b0 <= ib;
-      next_a0 <= ia;
+      c0 <= ic;
+      b0 <= ib;
+      a0 <= ia + k0 & 32'h000000FF;
     end
   endcase
 end
@@ -259,12 +203,12 @@ assign w[0] = key_length;
 genvar i;
 generate
   for (i=1; i<nloop+1; i=i+1) begin :loop
-    hash_r1 round (CLK, RST,a[i-1], b[i-1], c[i-1], k0, k1, k2, w[i-1], a[i], b[i], c[i], w[i]);
+    hash_r1 round (CLK, RST,a[i-1], b[i-1], c[i-1], w[i-1], k0, k1, k2, a[i], b[i], c[i], w[i]);
   end
 endgenerate
 
 /* last round */
-hash_r2 lastround (CLK, RST, a[nloop], b[nloop], c[nloop], k0, k1, k2, w[nloop], lc);
+hash_r2 lastround (CLK, RST, a[nloop], b[nloop], c[nloop], w[nloop], k0, k1, k2, lc);
 
 always @(posedge CLK) begin
   if (RST)
